e27f144d4b36a963d8a8206538f9eab613bafad6
actuallyCodeAlex <actuallyCodeAlex@gmail.com>
2023-05-26 08:33:16 -0700

    Sanity Studio Template Starter

---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'babbletron-3000-be',
+
+  projectId: '2wj8k8pe',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
---
+++ schemas/project.ts
@@ -0,0 +1,27 @@
+export default {
+  name: 'project',
+  type: 'document',
+  title: 'Project',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description',
+    },
+    {
+      name: 'color',
+      type: 'string',
+      title: 'Color',
+    },
+    {
+      name: 'url',
+      type: 'string',
+      title: 'URL',
+    },
+  ],
+}
